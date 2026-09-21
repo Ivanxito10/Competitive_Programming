f18cedc32339845a72026b57b96303b4e261287e
Ivan Torres <torresivan794@outlook.com>
2026-09-20 19:11:35 -0600

    Primer contest solucion problema B

--- contest1/B.cpp
+++ contest1/B.cpp
@@ -3,23 +3,36 @@ using namespace std;
 
 int main(){
     int t, n ,x;
-    cin >> t >>n;
-    
-    
-    vector <int> p(2*n);
+    cin >> t;
+    while(t--){
+
+    cin >> n;
+    vector <int> p(2*n +1);
+    int frec [2*n + 1] = {};
     
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++)
         {
             cin >> x;
-            p[i+j] = x; 
+            p[i+j] = x;
+            frec[x] = 1; 
         }
-        
     }
+
+    for (int i = 1; i <= 2* n; i++)
+    {
+        if(frec [i] == 0){
+            p[1] = i;
+            break; 
+        }
+    }
+    
+
     for (int i = 1; i <= 2*n; i++)
     {
         cout << p[i] << '\t';
+    }
     }
     return 0;
 }
